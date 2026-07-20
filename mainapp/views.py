3f78ea84987dc7488ca00edb9078421717cb73a6
Kiran Verma <kiranverma321890@gmail.com>
2026-07-21 01:27:25 +0530

    Initial commit

---
+++ mainapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
+
